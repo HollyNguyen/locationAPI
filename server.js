66c84e7c3b881c93badd9002d6b764635e6ee251
Forrest Cinelli <fccinelli@wpi.edu>
2017-09-18 13:05:16 -0400

    get rid of old way to handle data

--- server.js
+++ server.js
@@ -30,21 +30,6 @@ app.get('/search', handleSearch)
 app.listen(port);
 console.log('listening on ' + port)
 
-
-
-function getData() {
-  var file = new File(txtFile);
-  file.open("r"); // open file with read access
-  var out = [];
-  while (!file.eof) {
-    // read each line of text
-    out.push(file.readln().split(','));
-  }
-  file.close();
-  return JSON.stringify(out);
-}
-data = getData();
-
 function handleSearch(req, res) {
   let query = req.query
   for (let attr in query) {
